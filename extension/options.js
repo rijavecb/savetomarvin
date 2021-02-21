4eea92b3b1c73285b5681ec76abf886476b241f4
Branimir Rijavec <branko@rijavecb.com>
2021-02-21 13:21:47 +0100

    Sync settings of new options to Chrome Storage

--- extension/options.js
+++ extension/options.js
@@ -1,6 +1,18 @@
 function saveSettings() {
   const apiKey = document.getElementById('apiKey').value;
-  chrome.storage.sync.set({apiKey: apiKey}, function() {
+  const scheduleButtons = document.getElementById('scheduleButtons').checked;
+  const planningButtons = document.getElementById('planningButtons').checked;
+  const timeEstimateButtons = document.getElementById('timeEstimateButtons').checked;
+  const priorityButtons = document.getElementById('priorityButtons').checked;
+  const rewardPointsButtons =document.getElementById('rewardPointsButtons').checked;
+  chrome.storage.sync.set({
+    apiKey: apiKey,
+    scheduleButtons: scheduleButtons,
+    planningButtons: planningButtons,
+    timeEstimateButtons: timeEstimateButtons,
+    priorityButtons: priorityButtons,
+    rewardPointsButtons: rewardPointsButtons
+  }, function() {
   })
 }
 
@@ -11,4 +23,29 @@ chrome.storage.sync.get('apiKey', function(data) {
   if (data.apiKey !== undefined) {
     document.getElementById('apiKey').value = data.apiKey;
   }
+});
+
+chrome.storage.sync.get('scheduleButtons', function(data) {
+  document.getElementById('scheduleButtons').checked;
+  data.scheduleButtons ? document.getElementById('scheduleButtons').checked = true : document.getElementById('scheduleButtons').checked = false;
+});
+
+chrome.storage.sync.get('planningButtons', function(data) {
+  document.getElementById('planningButtons').checked;
+  data.planningButtons ? document.getElementById('planningButtons').checked = true : document.getElementById('planningButtons').checked = false;
+});
+
+chrome.storage.sync.get('timeEstimateButtons', function(data) {
+  document.getElementById('timeEstimateButtons').checked;
+  data.timeEstimateButtons ? document.getElementById('timeEstimateButtons').checked = true : document.getElementById('timeEstimateButtons').checked = false;
+});
+
+chrome.storage.sync.get('priorityButtons', function(data) {
+  document.getElementById('priorityButtons').checked;
+  data.priorityButtons ? document.getElementById('priorityButtons').checked = true : document.getElementById('priorityButtons').checked = false;
+});
+
+chrome.storage.sync.get('rewardPointsButtons', function(data) {
+  document.getElementById('rewardPointsButtons').checked;
+  data.rewardPointsButtons ? document.getElementById('rewardPointsButtons').checked = true : document.getElementById('rewardPointsButtons').checked = false;
 });

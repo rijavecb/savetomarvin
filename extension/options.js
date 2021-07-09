4c28194d3c6dc0c21fc4debf4954dac3e427bc90
Branimir Rijavec <branko@rijavecb.com>
2021-07-09 10:58:51 +0200

    Refactored code

--- extension/options.js
+++ extension/options.js
@@ -1,51 +1,47 @@
+const submitButton = document.getElementById("submitButton");
+submitButton.addEventListener("click", saveSettings);
+
 function saveSettings() {
-  const apiKey = document.getElementById('apiKey').value;
-  const scheduleButtons = document.getElementById('scheduleButtons').checked;
-  const planningButtons = document.getElementById('planningButtons').checked;
-  const timeEstimateButtons = document.getElementById('timeEstimateButtons').checked;
-  const priorityButtons = document.getElementById('priorityButtons').checked;
-  const rewardPointsButtons =document.getElementById('rewardPointsButtons').checked;
-  chrome.storage.sync.set({
-    apiKey: apiKey,
-    scheduleButtons: scheduleButtons,
-    planningButtons: planningButtons,
-    timeEstimateButtons: timeEstimateButtons,
-    priorityButtons: priorityButtons,
-    rewardPointsButtons: rewardPointsButtons
-  }, function() {
-  })
+  const apiKey = document.getElementById("apiKey").value;
+  const scheduleButtons = document.getElementById("scheduleButtons").checked;
+  const planningButtons = document.getElementById("planningButtons").checked;
+  const timeEstimateButtons = document.getElementById("timeEstimateButtons").checked;
+  const priorityButtons = document.getElementById("priorityButtons").checked;
+  const rewardPointsButtons = document.getElementById("rewardPointsButtons").checked;
+
+  chrome.storage.sync.set(
+    {
+      apiKey: apiKey,
+      scheduleButtons: scheduleButtons,
+      planningButtons: planningButtons,
+      timeEstimateButtons: timeEstimateButtons,
+      priorityButtons: priorityButtons,
+      rewardPointsButtons: rewardPointsButtons,
+    },
+    () => {}
+  );
 }
 
-const submitButton = document.getElementById('submitButton');
-submitButton.addEventListener('click', saveSettings);
+// Setting the state of apiKey input field and toggle switches based on what's stored in Chrome
 
-chrome.storage.sync.get('apiKey', function(data) {
-  if (data.apiKey !== undefined) {
-    document.getElementById('apiKey').value = data.apiKey;
-  }
+chrome.storage.sync.get(null, (items) => {
+  const allKeys = Object.keys(items);
+  allKeys.forEach((element) => {
+    element === "apiKey" ? getApiKey() : setSwitchValue(element);
+  });
 });
 
-chrome.storage.sync.get('scheduleButtons', function(data) {
-  document.getElementById('scheduleButtons').checked;
-  data.scheduleButtons ? document.getElementById('scheduleButtons').checked = true : document.getElementById('scheduleButtons').checked = false;
-});
+function getApiKey() {
+  chrome.storage.sync.get("apiKey", function (data) {
+    if (data.apiKey !== undefined)
+      document.getElementById("apiKey").value = data.apiKey;
+  });
+}
 
-chrome.storage.sync.get('planningButtons', function(data) {
-  document.getElementById('planningButtons').checked;
-  data.planningButtons ? document.getElementById('planningButtons').checked = true : document.getElementById('planningButtons').checked = false;
-});
-
-chrome.storage.sync.get('timeEstimateButtons', function(data) {
-  document.getElementById('timeEstimateButtons').checked;
-  data.timeEstimateButtons ? document.getElementById('timeEstimateButtons').checked = true : document.getElementById('timeEstimateButtons').checked = false;
-});
-
-chrome.storage.sync.get('priorityButtons', function(data) {
-  document.getElementById('priorityButtons').checked;
-  data.priorityButtons ? document.getElementById('priorityButtons').checked = true : document.getElementById('priorityButtons').checked = false;
-});
-
-chrome.storage.sync.get('rewardPointsButtons', function(data) {
-  document.getElementById('rewardPointsButtons').checked;
-  data.rewardPointsButtons ? document.getElementById('rewardPointsButtons').checked = true : document.getElementById('rewardPointsButtons').checked = false;
-});
+function setSwitchValue(element) {
+  chrome.storage.sync.get(element, function (data) {
+    data[element]
+      ? (document.getElementById(element).checked = true)
+      : (document.getElementById(element).checked = false);
+  });
+}
